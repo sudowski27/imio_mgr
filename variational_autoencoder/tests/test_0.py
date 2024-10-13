46e4c4fc3e8487074e400e807931215002dfd525
sudowski27 <Sudowski@o2.pl>
2024-10-13 21:31:24 +0200

    Add test_0 in version 0.1.0

---
+++ variational_autoencoder/tests/test_0.py
@@ -0,0 +1,37 @@
+"""version 0.1.0"""
+import pytest
+from variational_autoencoder.src.load_configuration import load_configuration
+from .helpers.create_json_configure_file import create_json_configure_file
+
+
+def test_0():
+    """
+    Test 0
+
+    Test loading good JSON file
+    """
+    configure_filename = "config.json"
+    expected_config = create_json_configure_file(configure_filename)
+
+    test_result = load_configuration(
+        configure_filename
+    )
+    assert test_result == expected_config
+
+
+def test_1():
+    """
+    Test 1
+
+    Test loading bad JSON file
+    """
+    configure_filename = "config.json"
+    create_json_configure_file(
+        configure_filename,
+        is_json_syntax_good=False
+    )
+
+    with pytest.raises(ValueError):
+        load_configuration(
+            configure_filename
+        )
