3414d4ccfa87421f213a1cf561ba98bbce1ac892
sudowski27 <Sudowski@o2.pl>
2024-10-14 23:13:51 +0200

    Change file main.py to version 0.1.1

--- variational_autoencoder/main.py
+++ variational_autoencoder/main.py
@@ -1,11 +1,13 @@
-"""version 0.1.0"""
+"""version 0.1.1"""
+from .src.load_configuration import load_configuration
 
 
 def main() -> None:
     """
     Main function
     """
-    pass
+    configure_file = "configuration.json"
+    configuration = load_configuration(configure_file)
 
 
 if __name__ == "__main__":
