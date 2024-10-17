0531b0af9bfb2f3d48881f2ce49b94390002cfac
sudowski27 <Sudowski@o2.pl>
2024-10-17 20:18:34 +0200

    Update tests/test_17.py to version 0.1.1
    Update test_0

--- variational_autoencoder/tests/test_17.py
+++ variational_autoencoder/tests/test_17.py
@@ -1,11 +1,11 @@
-"""version 0.1.0"""
+"""version 0.1.1"""
 from variational_autoencoder.src.data_dimensions import (
     load_dataframe,
     validate_dataframe,
     is_devices_seperated,
     connect_devices,
     get_first_device,
-    count_unique_gate_drain_voltages
+    count_unique_gate_source_voltages
 )
 from .helpers.create_csv_train_dataset_file import create_csv_train_dataset_file, CreateCsvTrainDatasetFileArguments
 from .helpers.remove_file import remove_file
@@ -18,12 +18,12 @@ def test_0():
     Test connecting devices
     """
     filename = "text_csv.csv"
-    count_unique_gate_drain_voltages_expected = 20
+    count_unique_gate_source_voltages_expected = 20
     args = CreateCsvTrainDatasetFileArguments(
         "text_csv.csv",
         5,
         4,
-        count_unique_gate_drain_voltages_expected,
+        count_unique_gate_source_voltages_expected,
         5,
         True
     )
@@ -34,6 +34,6 @@ def test_0():
     assert is_devices_seperated(dataframe)
     connected_devices_dataframe = connect_devices(dataframe)
     first_device_dataframe = get_first_device(connected_devices_dataframe)
-    count_unique_gate_drain_voltages_value = count_unique_gate_drain_voltages(first_device_dataframe)
-    assert count_unique_gate_drain_voltages_value == count_unique_gate_drain_voltages_expected
+    count_unique_gate_source_voltages_value = count_unique_gate_source_voltages(first_device_dataframe)
+    assert count_unique_gate_source_voltages_value == count_unique_gate_source_voltages_expected
     remove_file(filename)
