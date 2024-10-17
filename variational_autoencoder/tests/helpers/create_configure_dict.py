14cc3893575f6e80ac0b7330fa7447ae1c5dbaff
sudowski27 <Sudowski@o2.pl>
2024-10-18 00:00:37 +0200

    Update tests/helpers/create_configure_dict.py to version 0.1.2
    update function create_configure_dict

--- variational_autoencoder/tests/helpers/create_configure_dict.py
+++ variational_autoencoder/tests/helpers/create_configure_dict.py
@@ -1,4 +1,4 @@
-"""version 0.1.1"""
+"""version 0.1.2"""
 from variational_autoencoder.src.configuration_keys import configuration_keys
 
 
@@ -22,6 +22,8 @@ def create_configure_dict(**kwargs) -> dict:  # pylint: disable=R0912, R0915
         synth_data_file_name_core_value: bool
         batch_size_key: bool
         batch_size_value: bool
+        epochs_key: bool
+        epochs_value: bool
         learning_rate_key: bool
         learning_rate_value: bool
         wkl_key: bool
@@ -41,6 +43,7 @@ def create_configure_dict(**kwargs) -> dict:  # pylint: disable=R0912, R0915
         configuration_keys.synth_data_folder: "./mosfet_dc_synthetic/trained_on_50",
         configuration_keys.synth_data_file_name_core: "nmos_dc_synth",
         configuration_keys.batch_size: 25,
+        configuration_keys.epochs: 40000,
         configuration_keys.learning_rate: 1e-4,
         configuration_keys.wkl: 0.01,
         configuration_keys.device: "cuda"
@@ -104,6 +107,15 @@ def create_configure_dict(**kwargs) -> dict:  # pylint: disable=R0912, R0915
         if kwargs["batch_size_value"]:
             if configuration_keys.batch_size in bad_configure_dict:
                 bad_configure_dict[configuration_keys.batch_size] = "aaa"
+
+    if "epochs_key" in kwargs:
+        if kwargs["epochs_key"]:
+            bad_configure_dict.pop(configuration_keys.epochs, None)
+
+    if "epochs_value" in kwargs:
+        if kwargs["epochs_value"]:
+            if configuration_keys.epochs in bad_configure_dict:
+                bad_configure_dict[configuration_keys.epochs] = "aaa"
 
     if "learning_rate_key" in kwargs:
         if kwargs["learning_rate_key"]:
