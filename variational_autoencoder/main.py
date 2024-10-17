a948eba64e378a36a86ac23419582bf51d01608a
sudowski27 <Sudowski@o2.pl>
2024-10-17 23:15:08 +0200

    Update main.py to version 0.1.2

--- variational_autoencoder/main.py
+++ variational_autoencoder/main.py
@@ -1,5 +1,8 @@
-"""version 0.1.1"""
-from .src.load_configuration import load_configuration
+"""version 0.1.2"""
+from src.load_configuration import load_configuration
+from src.validate_configuration import validate_configuration
+from src.data_dimensions import data_dimensions
+from src.configuration_keys import configuration_keys
 
 
 def main() -> None:
@@ -8,6 +11,11 @@ def main() -> None:
     """
     configure_file = "configuration.json"
     configuration = load_configuration(configure_file)
+    validate_configuration(configuration)
+
+    (channel_width_dim,
+     gate_source_dim,
+     drain_source_dim) = data_dimensions(configuration[configuration_keys.train_dataset_file])
 
 
 if __name__ == "__main__":
