3cc43569b51ed3ce1364a98181992738fdc1a2ab
sudowski27 <Sudowski@o2.pl>
2024-10-18 12:20:41 +0200

    Add tests/test_23.py

---
+++ variational_autoencoder/tests/test_23.py
@@ -0,0 +1,53 @@
+"""version 0.1.0"""
+from variational_autoencoder.src.configuration_keys import configuration_keys
+from variational_autoencoder.src.get_train_dataframe import get_train_dataframe_filename
+from .helpers.create_csv_train_dataset_file import create_csv_train_dataset_file, CreateCsvTrainDatasetFileArguments
+from .helpers.remove_file import remove_file
+
+
+def test_0(small_configure_with_train_dataset_file):
+    """
+    Test 0
+
+    Test load train dataframe with devices together
+    """
+    configure = small_configure_with_train_dataset_file
+    filemame = configure[configuration_keys.train_dataset_file]
+    args = CreateCsvTrainDatasetFileArguments(
+        filemame,
+        5,
+        4,
+        10,
+        20,
+        False
+    )
+    expected_dataframe = create_csv_train_dataset_file(args)
+
+    train_dataframe = get_train_dataframe_filename(filemame)
+
+    assert train_dataframe.equals(expected_dataframe)
+    remove_file(filemame)
+
+
+def test_1(small_configure_with_train_dataset_file):
+    """
+    Test 1
+
+    Test load train dataframe with devices seperated
+    """
+    configure = small_configure_with_train_dataset_file
+    filemame = configure[configuration_keys.train_dataset_file]
+    args = CreateCsvTrainDatasetFileArguments(
+        filemame,
+        5,
+        4,
+        10,
+        20,
+        True
+    )
+    expected_dataframe = create_csv_train_dataset_file(args)
+
+    train_dataframe = get_train_dataframe_filename(filemame)
+
+    assert train_dataframe.equals(expected_dataframe)
+    remove_file(filemame)
